2c522c43b619138ce15b53f7b94f3a010cca6399
Philip <philip@shiftyjelly.com.au>
2012-07-05 17:30:45 +0930

    Fixed group messages that don't get sent if no devices linked

--- c2dm_on_rails.gemspec
+++ c2dm_on_rails.gemspec
@@ -5,11 +5,11 @@
 
 Gem::Specification.new do |s|
   s.name = "c2dm_on_rails"
-  s.version = "0.3.0"
+  s.version = "0.3.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Julius de Bruijn"]
-  s.date = "2012-07-04"
+  s.date = "2012-07-05"
   s.description = "C2DM on Rails is a Ruby on Rails gem that allows you to\neasily add Android Cloud to Device Messaging support to your Rails application.\n"
   s.email = "julius.debruijn@digia.com"
   s.extra_rdoc_files = [
--- lib/c2dm_on_rails/app/models/c2dm/group.rb
+++ lib/c2dm_on_rails/app/models/c2dm/group.rb
@@ -12,14 +12,21 @@ class C2dm::Group < C2dm::Base
     return if self.unsent_group_notifications.nil?
           
     self.unsent_group_notifications.each do |notification|
+      
+      devices_exist = false
+      
       # api only allows you to send to 1000 devices at once
       self.devices.select("registration_id").find_in_batches(:batch_size => 1000) do |device_batch|
+        devices_exist = true unless devices_exist
         registration_ids = device_batch.collect { |device| device.registration_id }
         next if registration_ids.blank?
         logger.info "sending notification #{notification.id} to devices #{registration_ids}"
         response = C2dm::Connection.send_group_notification(notification, registration_ids)
         logger.info "response: #{response[:code]}; #{response.inspect}"
         if response[:code] == 200
+          
+          notification.sent_at = Time.now
+          notification.save!
           
           # TODO figure out how to handle the response correctly
           
@@ -44,8 +51,8 @@ class C2dm::Group < C2dm::Base
             ex = C2dm::Errors::MissingCollapseKey.new(response[:message])
             logger.warn(ex.message)
           else
-            notification.sent_at = Time.now
-            notification.save!
+            # notification.sent_at = Time.now
+            # notification.save!
           end
         elsif response[:code] == 503
           raise C2dm::Errors:ServiceUnavailable.new(response[:message])
@@ -54,6 +61,13 @@ class C2dm::Group < C2dm::Base
         else
         end
       end
+      
+      # mark the notification as sent if no devices are associated
+      unless devices_exist
+        notification.sent_at = Time.now
+        notification.save!
+      end
+      
     end
   end
 end
